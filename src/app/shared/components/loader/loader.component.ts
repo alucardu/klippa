b29375e207488d0f9ccecd5b77de2c901b5ab78e
Peter Boomsma <peter@cleanpixel.nl>
2021-11-15 02:25:52 +0100

    Styled the view files components

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input  } from '@angular/core';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.sass']
+  styleUrls: ['./loader.component.scss']
 })
 
 export class LoaderComponent implements OnInit {
--- src/app/shared/components/view-files/view-files.component.ts
+++ src/app/shared/components/view-files/view-files.component.ts
@@ -1,22 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import { AppState, Receipt } from 'src/app/models/klippa.models';
 import { selectReceipts } from 'src/app/state/file-state/file-state.selectors';
 @Component({
   selector: 'app-view-files',
   templateUrl: './view-files.component.html',
-  styleUrls: ['./view-files.component.sass']
+  styleUrls: ['./view-files.component.scss']
 })
 
 export class ViewFilesComponent implements OnInit {
   receipts$: Observable<Receipt[]>;
 
-  constructor(private store: Store<AppState>) {
+  constructor(
+    public dialog: MatDialog,
+    private store: Store<AppState>
+  ) {
     this.receipts$ = this.store.pipe(select(selectReceipts))
   }
 
   ngOnInit(): void {}
 
+  openDialog(receipt: Receipt) {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+      data: {receipt: receipt}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+}
+
+@Component({
+  selector: 'app-view-file',
+  templateUrl: './view-file.component.html',
+  styleUrls: ['./view-file.component.scss']
+})
+export class DialogContentExampleDialog {
+  receipt?: Receipt
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogContentExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {
+    this.receipt = data.receipt
+    console.log(this.receipt)
+  }
 }
